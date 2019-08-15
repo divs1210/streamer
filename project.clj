f81f46fbd037a54e047d03e6f9d8a5fc06c398c4
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-08-16 00:37:40 +0530

    Add cloverage plugin, codecov badge

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [net.cgrand/xforms "0.17.0"]])
+                 [net.cgrand/xforms "0.17.0"]]
+  :profiles {:dev {:plugins [[lein-cloverage "1.1.1"]]}})
